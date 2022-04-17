97d562a0fab27da51ad65c9370d4225ee422b726
mireyn <mir3yn@gmail.com>
2022-04-17 11:41:48 +0300

    menu, health item scripts & some extension methods

---
+++ game/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Вспомогательный класс преобразований для объектов
+/// </summary>
+public static class ExtensionMethods {
+	/// <summary>
+	/// Преобразование типа Vector3 в тип Vector2
+	/// </summary>
+	/// <param name="vec3">Изначальный 3-вектор, который будет преобразован</param>
+	public static Vector2 toVector2(this Vector3 vec3) {
+		return new Vector2(vec3.x, vec3.y);
+	}
+}
---
+++ game/Assets/Scripts/HealthPickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Класс полезного предмета, восстанавливающего здоровье
+/// </summary>
+public class HealthPickUp : MonoBehaviour {
+	/// <summary>
+	/// Количество восстанавливаемого здоровья
+	/// </summary>
+	public float healthAmount;
+
+    /// <summary>
+    /// Срабатывание триггера при столкновении с ним. Игрок восстанавливает количество здоровья, равное healthAmount
+    /// </summary>
+    /// <param name="coll">Коллайдер объекта, с которым произошло столкновение</param>
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.tag == "Player") {
+			PlayerHealth playerHealth  = coll.gameObject.GetComponent<PlayerHealth>();
+			playerHealth.addHealth(healthAmount);
+			Destroy(gameObject);
+		}
+	}
+}
